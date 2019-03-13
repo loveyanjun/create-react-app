2c6c55d30bf50251bddb9721600a82deaed3e062
loveyanjun <1270851085@qq.com>
2019-03-13 17:37:21 +0800

    redux学习

--- src/projects/test-redux/components/todoList.js
+++ src/projects/test-redux/components/todoList.js
@@ -1,37 +1,51 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import todos from '../../../redux/reducers/todos'
-import { getTodosByVisibilityFilter } from '../../../redux/selectors'
+// import todos from '../../../redux/reducers/todos'
+// import { getTodosByVisibilityFilter } from '../../../redux/selectors'
 import '../styles/todo-list.css'
 
 class TodoList extends React.PureComponent {
+    static propTypes = {
+        todoList: PropTypes.array
+    }
+
     render () {
+        const { todoList } = this.props
+        console.log(todoList)
         return (
             <div className='todo-list'>
                 <h1>列表展示：</h1>
-
+                {todoList && todoList.length > 0 ?
+                    todoList.map(({id, content}) => (
+                        <div key={id}>
+                            <p>列表id:{id}</p>
+                            <p>列表内容:{content}</p>
+                        </div>
+                    )) : null}
             </div>
         )
     }
 }
 
+// const TodoList = ({todoList}) => (
+//     <ul className="todo-list">
+//       {todoList && todoList.length
+//         ? todoList.map(({id, content}) => {
+//             return (
+//                 <div key={id}>{content}</div>
+//             )
+//           })
+//         : "No todos, yay!"}
+//     </ul>
+//   );
+
 const mapStateToProps = state => {
-    // const { todoList, allIds } = state.todos || {}
-    // const todos = allIds && allIds.length ? allIds.map(id => (todoList ? { ...todoList, id } : null)) : null
-    // console.log('tttttttt')
-    // console.log(todos)
-    // return { todos }
-    const { visibilityFilter } = state
-    // console.log('state中的todos')
-    console.log(state.todos)
-    // const todos = getTodosByVisibilityFilter(state, visibilityFilter)
+    // const { visibilityFilter } = state
     const { todoList } = state.todos
-    console.log('todo:')
-    console.log(todoList)
     return {
         todoList
     }
-    // return { todos }
 }
 
 export default connect(mapStateToProps)(TodoList)
--- src/redux/reducers/todos.js
+++ src/redux/reducers/todos.js
@@ -2,23 +2,29 @@ import { ADD_TODO } from '../actionTypes'
 
 const initialState = {
     allIds: [],
-    todoList: {}
+    todoList: []
 }
 
 export default function (state = initialState, action) {
     switch (action.type) {
         case ADD_TODO: {
             const { id, content } = action.payload
+            console.log(state)
             return {
                 ...state,
                 allIds: [...state.allIds, id],
-                todoList: {
-                    ...state.todoList,
-                    [id]: {
-                        content,
-                        completed: false
-                    }
-                }
+                // todoList: {
+                //     ...state.todoList,
+                //     [id]: {
+                //         content,
+                //         completed: false
+                //     }
+                // }
+                todoList: [{
+                    id: content,
+                    content,
+                    completed: false
+                }]
             }
         }
         default:
